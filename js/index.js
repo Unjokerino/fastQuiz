26d7eddb358222c997fbf14b514f7a5d79a33118
Unjokerino <35525106+Unjokerino@users.noreply.github.com>
2020-01-27 17:02:35 +0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,53 @@
+let page = 1;
+let pages = document.querySelectorAll(".page");
+let pages_length = pages.length;
+
+let next_btn = document.querySelector("#next");
+
+next_btn.addEventListener("click", () => next());
+
+next = () => {
+  incPage();
+};
+
+incPage = () => {
+  if (page < pages_length) {
+    pages.forEach((element, index) => {
+      pages[index].style.display = "none";
+    });
+    pages[page].style.display = "block";
+    page++;
+  } else {
+    page = 0;
+    let repeat_btn = document.createElement("button");
+    repeat_btn.innerHTML = "Повторить";
+    repeat_btn.id = "repeat";
+    repeat_btn.addEventListener("click", () => {
+      location.reload();
+    });
+    loader = document.createElement("div");
+    loader.classList.add("loader");
+    loader.innerHTML = `<div id='c'>
+    <div class='s'></div>
+  </div>`;
+    document.querySelector(".container").innerHTML = "";
+    document.querySelector(".container").append(loader);
+
+    setTimeout(() => {
+      document.querySelector("#c").remove();
+      showSnackBar(
+        "К сожалению рядом с вами никого не найдено, попробуйте позже!"
+      );
+      loader.append(repeat_btn);
+    }, 5000);
+  }
+};
+
+function showSnackBar(message) {
+  var x = document.getElementById("snackbar");
+  x.innerHTML = message;
+  x.className = "show";
+  setTimeout(function() {
+    x.className = x.className.replace("show", "");
+  }, 3000);
+}
